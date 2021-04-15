0c9297c0d14db82c6cfe01f439868ab27ba51b33
Cory <corys4139@gmail.com>
2021-04-15 12:17:26 -0400

    establish environmental variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,8 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n!r+m@k+%xzsi4!e*y$85q)x*1o^u4ly+3t8k+vt1)xjq20)gs'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
