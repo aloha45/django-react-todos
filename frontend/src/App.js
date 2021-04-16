9d20166d075ff416bc50bd0697af87019e37f155
Cory <corys4139@gmail.com>
2021-04-16 12:52:42 -0400

    create testing frontend form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,6 @@
 import React, { Component } from "react";
+import Form from './components/form'
 import "./App.css";
-
-const todoItems = [
-	{
-		id: 1,
-		title: "Go to Market",
-		description: "Buy ingredients to prepare dinner",
-		completed: true,
-	},
-	{
-		id: 2,
-		title: "Study",
-		description: "Read Algebra and History textbook for the upcoming test",
-		completed: false,
-	},
-	{
-		id: 3,
-		title: "Sammy's books",
-		description: "Go to library to return Sammy's books",
-		completed: true,
-	},
-	{
-		id: 4,
-		title: "Article",
-		description: "Write article on how to use Django with React",
-		completed: false,
-	},
-];
-
 class App extends Component {
 	constructor(props) {
 		super(props);
@@ -69,11 +42,12 @@ class App extends Component {
 				<main className="container">
 					<h1>Todo app</h1>
 					<button className="btn btn-primary">Add task</button>
-					<div className="todos">
+					<ul className="todos">
 						{this.state.todoList.map((todo) => 
-							<p>{todo.title}</p>
+							<li>{todo.title}</li>
 						)}
-					</div>
+					</ul>
+          <Form />
 				</main>
 			</>
 		);
---
+++ frontend/src/components/form.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+export default class Form extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			activeItem: {
+				title: "",
+				description: "",
+				completed: false,
+			},
+		};
+	}
+
+	handleChange = (e) => {
+		let { name, value } = e.target;
+
+		if (e.target.type === "checkbox") {
+			value = e.target.checked;
+		}
+
+		const activeItem = { ...this.state.activeItem, [name]: value };
+
+		this.setState({ activeItem });
+	};
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("saved")
+    alert("saved" + JSON.stringify(this.state.activeItem))
+  }
+
+	render() {
+		return (
+			<>
+				<h2>Todo Item</h2>
+				<form>
+					<label for="todo-title">Title</label>
+					<input
+						type="text"
+						id="todo-title"
+						name="title"
+						value={this.state.activeItem.title}
+						onChange={this.handleChange}
+						placeholder="Enter Todo Title"
+					/>
+					<label for="todo-description">Description</label>
+					<input
+						type="text"
+						id="todo-description"
+						name="description"
+						value={this.state.activeItem.description}
+						onChange={this.handleChange}
+						placeholder="Enter Todo description"
+					/>
+					<label for="completed">Completed?</label>
+					<input
+						type="checkbox"
+						name="completed"
+						checked={this.state.activeItem.completed}
+						onChange={this.handleChange}
+					/>
+					Completed
+					<button color="success" onClick={this.handleSubmit}>
+						Save
+					</button>
+				</form>
+			</>
+		);
+	}
+}
